6aabb6873497c64ee1711ec7cac5298f6da55513
Prakhar Pandey <prakharpandeylfs@gmail.com>
2024-01-13 04:40:40 +0530

    first commit

--- app/done/button.tsx
+++ app/done/button.tsx
@@ -1,10 +1,11 @@
 "use client"
 import React from 'react'
-const Button = ({deleteTodo}) => {
-  return (
-    <button className="border border-slate-300 text-blue-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-    onClick={() => deleteTodo()}>Delete</button>
-  )
+const Button = ({ deleteTodo }: { deleteTodo: () => void }) => {
+    return (
+        <button className="border border-slate-300 text-blue-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+            onClick={() => deleteTodo()}>Delete</button>
+    )
 }
+
 
 export default Button
